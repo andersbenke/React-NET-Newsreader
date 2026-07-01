9cc7f380e67389d7ab10dc4903716639dfd37e98
Anders Benke <anders.benke@gmail.com>
2026-07-01 16:04:22 +0200

    Now we can talk to the NewsAPI.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,9 +13,38 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/newscategoriesfromprompt", () =>
+app.MapGet("/news", () =>
 {
-	return "En stor hest";
+	// init with your API key
+	Console.WriteLine("API KEY " + app.Configuration["NEWSAPI-API-KEY"].Length);
+
+	var newsApiClient = new NewsApiClient(app.Configuration["NEWSAPI-API-KEY"]);
+	var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+	{
+		Q = "hundar"
+	});
+	if (articlesResponse.Status == Statuses.Ok)
+	{
+		// total results found
+		Console.WriteLine(articlesResponse.TotalResults);
+		// here's the first 20
+		foreach (var article in articlesResponse.Articles)
+		{
+			// title
+			Console.WriteLine(article.Title);
+			// author
+			Console.WriteLine(article.Author);
+			// description
+			Console.WriteLine(article.Description);
+			// url
+			Console.WriteLine(article.Url);
+			// published at
+			Console.WriteLine(article.PublishedAt);
+		}
+	} else
+	{
+		Console.WriteLine("ANALSEX");
+	}
 });
 
 app.Run();
