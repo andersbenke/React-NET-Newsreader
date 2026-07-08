4ee10608238b41dc8be9cbe3b0d51ee0f9674689
Anders Benke <anders.benke@gmail.com>
2026-07-08 14:35:03 +0200

    Add React to index.html and make nav element into React component.

---
+++ frontend/js/components/Nav.jsx
@@ -0,0 +1,14 @@
+function Nav() {
+  return(<nav aria-label="Categories">
+    <ul>
+      <li><a href="#" aria-current="page">top</a></li>
+      <li><a href="#">general</a></li>
+      <li><a href="#">business</a></li>
+      <li><a href="#">technology</a></li>
+      <li><a href="#">science</a></li>
+      <li><a href="#">health</a></li>
+      <li><a href="#">sports</a></li>
+      <li><a href="#">entertainment</a></li>
+    </ul>
+  </nav>);
+}
